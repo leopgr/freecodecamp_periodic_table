029a77e3e5bca9cf311922bd6b56102065fa02ef
leopgr <leonardo.pegoraro@gmail.com>
2022-07-27 21:07:30 +0000

    feat: add fn_get_element logic

--- element.sh
+++ element.sh
@@ -8,10 +8,62 @@ fi
 
 PSQL_COMM="/usr/bin/psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-function fn_get_element(){
-   ELEM_EXISTS=0 
+   re='^[0-9]+$'
+   if [[ $SCRIPT_PARAM =~ $re ]] ; then
+      # check if was provided atomic_number
+      QUERY="select count(*) from elements where atomic_number=$SCRIPT_PARAM"
+      ELEM_EXISTS=$($PSQL_COMM "$QUERY")
 
-}
+      if [ $ELEM_EXISTS -ne 0 ]; then
+         ATOMIC_NUMBER=$SCRIPT_PARAM
+      fi
+   fi
+   
+   # check if was provided symbol
+   if [ $ELEM_EXISTS -eq 0 ]; then
+      QUERY="select count(*) from elements where symbol='$SCRIPT_PARAM'"
+      ELEM_EXISTS=$($PSQL_COMM "$QUERY")
+
+      if [ $ELEM_EXISTS -ne 0 ]; then
+         QUERY="select atomic_number from elements where symbol='$SCRIPT_PARAM'"
+         ATOMIC_NUMBER=$($PSQL_COMM "$QUERY")
+      else
+        # check if was provided name
+         QUERY="select count(*) from elements where name='$SCRIPT_PARAM'"
+         ELEM_EXISTS=$($PSQL_COMM "$QUERY")
+
+         if [ $ELEM_EXISTS -ne 0 ]; then
+            QUERY="select atomic_number from elements where name='$SCRIPT_PARAM'"
+            ATOMIC_NUMBER=$($PSQL_COMM "$QUERY")
+         fi
+      fi
+   fi
+
+   if [ $ELEM_EXISTS -ne 0 ]; then
+      QUERY="select name from elements where atomic_number=$ATOMIC_NUMBER"
+      ELEMENT_NAME=$($PSQL_COMM "$QUERY")
+      
+      QUERY="select symbol from elements where atomic_number=$ATOMIC_NUMBER"
+      ELEMENT_SYMBOL=$($PSQL_COMM "$QUERY")
+
+      QUERY="select types.type from types join properties on types.type_id=properties.type_id \
+join elements on elements.atomic_number=properties.atomic_number where elements.atomic_number=$ATOMIC_NUMBER"
+      ELEMENT_TYPE=$($PSQL_COMM "$QUERY")
+
+      QUERY="select melting_point_celsius from properties where atomic_number=$ATOMIC_NUMBER"
+      MELTING_POINT=$($PSQL_COMM "$QUERY")
+
+      QUERY="select boiling_point_celsius from properties where atomic_number=$ATOMIC_NUMBER"
+      BOILING_POINT=$($PSQL_COMM "$QUERY")
+
+      QUERY="select atomic_mass::REAL from properties where atomic_number=$ATOMIC_NUMBER"
+      ATOMIC_MASS=$($PSQL_COMM "$QUERY")
+
+      echo -e "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. \
+$ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+   else
+      echo -e "I could not find that element in the database."
+   fi
 
 # main
 fn get_element
